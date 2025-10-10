76d8fe087cf49767fc71b481795dacf7bf9c7f9f
yeeun <drrrkak@gmail.com>
2025-10-10 16:10:24 +0900

    refactor : users Entity 탈퇴 관련 필드 추가

--- src/main/java/com/mutt/mutt_BE/user/domain/Users.java
+++ src/main/java/com/mutt/mutt_BE/user/domain/Users.java
@@ -8,9 +8,14 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 /**
  * 일반 사용자 엔티티
  * 주문, 장바구니, 리뷰, 위시리스트 등과 연관
+ *
+ * 탈퇴 관련 필드 :
+ * isActive = false , withdrawalAt ,withdrawal_reason
  *
  * @see Account
  */
@@ -22,10 +27,14 @@ import java.time.LocalDate;
 @SuperBuilder
 public class Users extends Account {
 
-    @Column(unique = true,length = 20)
+    @Column(unique = true, length = 20)
     private String phoneNumber;
 
     private String name;
+    private LocalDate birthDate;
 
-    private LocalDate birthDate; //
+    @Column(nullable = false)
+    private boolean isActive;
+    private LocalDateTime withdrawalAt;
+    private String withdrawal_reason;
 }
